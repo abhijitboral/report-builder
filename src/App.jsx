b7f7bc1ffeddc6ed8bf0e8378a404bfffa85b3c4
abhijit.boral@coudeclouds.in <abhijit1512.boral@gmail.com>
2025-04-28 23:03:25 +0530

    clear app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import DynamicHeader from "./components/DynamicHeader";
-import DynamicContent from "./components/DynamicContent";
-import DynamcLogin from "./components/DynamicLoging";
-import DynamcSignup from "./components/DynamicSignup";
-import DynamicLayout from "./components/DynamicLayout";
 import DynamicRouter from "./Routers/Router";
 function App() {
 	return (
 		<>
-			{/* <DynamcLogin />
-			<DynamcSignup /> */}
 			<BrowserRouter>
 				<DynamicRouter />
 			</BrowserRouter>
