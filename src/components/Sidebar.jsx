d7ffcfbb5e31cea48b6ba5711938efc848835bf9
abhijit.boral@coudeclouds.in <abhijit1512.boral@gmail.com>
2025-04-28 23:04:29 +0530

    clear DynamicMenu.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,27 +27,6 @@ const Sidebar = () => {
 				)}
 			</div>
 			<DynamicMenu />
-			{/* <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-						<MenuItem key="1" icon={<DashboardOutlined />}>
-							Dashboard
-						</MenuItem>
-						<MenuItem key="2" icon={<FunnelPlotOutlined />}>
-							Funnels
-						</MenuItem>
-						<MenuItem key="3" icon={<LineChartOutlined />}>
-							Reporting
-							<Menu.SubMenu key="sub1" title="Reporting">
-								<MenuItem key="4">Report Builder</MenuItem>
-								<MenuItem key="5">Campaign Analysis</MenuItem>
-								<MenuItem key="6">Raw Events</MenuItem>
-								<MenuItem key="7">Integrated Reporting</MenuItem>
-							</Menu.SubMenu>
-						</MenuItem>
-						<MenuItem key="8" icon={<SettingOutlined />}>
-							Data Updates
-						</MenuItem>
-						<MenuItem key="9">Labs</MenuItem>
-					</Menu> */}
 		</Sider>
 	);
 };
